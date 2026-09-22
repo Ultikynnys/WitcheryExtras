4071ff38df92ba582a65fbc2091c3a62175b455b
Ultikynnys <Ultikynnys@users.noreply.github.com>
2026-09-22 23:17:02 +0300

    add missing Shadow import

--- src/main/java/alkalus/main/mixins/late/witchery/ShapeshiftMixin.java
+++ src/main/java/alkalus/main/mixins/late/witchery/ShapeshiftMixin.java
@@ -1,6 +1,7 @@
 package alkalus.main.mixins.late.witchery;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
