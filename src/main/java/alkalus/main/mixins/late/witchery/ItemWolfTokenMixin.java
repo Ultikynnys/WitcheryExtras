e0bf2373109d1e1b4547ebc1112fcef259dacf40
Ultikynnys <Ultikynnys@users.noreply.github.com>
2026-09-23 00:44:42 +0300

    fix missing Unique/Constant imports

--- src/main/java/alkalus/main/mixins/late/witchery/ItemWolfTokenMixin.java
+++ src/main/java/alkalus/main/mixins/late/witchery/ItemWolfTokenMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
